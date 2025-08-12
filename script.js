31a3e0272efd85f716a2c427741f38c27eda74f1
Bassam-Radwan <bradwanbassam@gmail.com>
2025-08-12 16:52:29 +0100

    Level-300

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const state = {
   selectedEpisode: null,
   searchTerm: "",
 };
+const manyEpisodes = document.getElementById("manyEpisodes");
 
-function setup() {
-  state.allEpisodes = getAllEpisodes();
+async function setup() {
+  state.allEpisodes = await start();
   state.filteredEpisodes = [...state.allEpisodes];
-
   populateEpisodeSelector(state.allEpisodes);
   renderEpisodes(state.filteredEpisodes);
 
@@ -17,6 +17,7 @@ function setup() {
 
   const episodeSelector = document.getElementById("episodeSelector");
   episodeSelector.addEventListener("change", handleSelect);
+  tvShowType(state.allEpisodes)
 }
 
 function handleSearch(event) {
@@ -25,9 +26,10 @@ function handleSearch(event) {
 
   state.filteredEpisodes = state.allEpisodes.filter((ep) =>
     ep.name.toLowerCase().includes(term) ||
-    ep.summary.toLowerCase().includes(term) ||
-    ep.season.toString().includes(term) ||
-    ep.number.toString().includes(term)
+    ep.summary.toLowerCase().includes(term)
+    // ep.season.toString().includes(term) ||
+    // ep.number. includes(term)
+
   );
 
   state.selectedEpisode = null;
@@ -40,15 +42,19 @@ function handleSelect(event) {
   if (value === "all") {
     state.selectedEpisode = null;
     renderEpisodes(state.filteredEpisodes);
+
   } else {
     state.selectedEpisode = state.allEpisodes.find(
       (ep) => getEpisodeOptionValue(ep) === value
     );
     renderEpisodes([state.selectedEpisode]);
+
   }
 }
 
 function renderEpisodes(episodes) {
+  manyEpisodes.textContent = `${episodes.length} of ${state.allEpisodes.length}`;
+
   const main = document.getElementById("root");
   main.innerHTML = "";
 
@@ -60,7 +66,9 @@ function renderEpisodes(episodes) {
     heading.classList.add("heading");
 
     const title = document.createElement("h2");
-    title.textContent = `${ep.name} - S${String(ep.season).padStart(2, "0")}E${String(ep.number).padStart(2, "0")}`;
+    title.textContent = ep.number != null
+      ? `${ep.name} - S${String(ep.season).padStart(2, "0")}E${String(ep.number).padStart(2, "0")}`
+      : `${ep.name}  `;
 
     heading.appendChild(title);
     container.appendChild(heading);
@@ -83,6 +91,42 @@ function renderEpisodes(episodes) {
   });
 }
 
+// this is  a feature for level 400 
+function tvShowType(episodes) {
+  let selectedShow = [];
+  let genresType = [];
+  let showType = document.getElementById('showType');
+  // create option elements 
+  episodes.forEach(episode => {
+    episode.genres.forEach(genre => {
+      if (!genresType.includes(genre)) {
+        genresType.push(genre)
+        let option = document.createElement('option')
+        option.value = genre
+        option.textContent = genre
+        showType.appendChild(option)
+      }
+    })
+  })
+  // render selected type
+  showType.addEventListener('change', () => {
+    selectedShow.splice(0, selectedShow.length)
+    episodes.forEach(episode => {
+      episode.genres.forEach(genre => {
+
+        if (showType.value === genre) {
+          selectedShow.push(episode)
+        }
+      })
+      renderEpisodes(selectedShow)
+    }
+    )
+    if (showType.value === 'all') {
+      renderEpisodes(episodes)
+    }
+  }
+  )
+}
 function populateEpisodeSelector(episodes) {
   const selector = document.getElementById("episodeSelector");
   selector.innerHTML = "";
@@ -95,13 +139,55 @@ function populateEpisodeSelector(episodes) {
   episodes.forEach((ep) => {
     const option = document.createElement("option");
     option.value = getEpisodeOptionValue(ep);
-    option.textContent = `S${String(ep.season).padStart(2, "0")}E${String(ep.number).padStart(2, "0")} - ${ep.name}`;
+    option.textContent = ep.number != null ? `S${String(ep.season).padStart(2, "0")}
+    E${String(ep.number).padStart(2, "0")} - ${ep.name}`
+      : ep.name;
     selector.appendChild(option);
   });
 }
 
+// 
 function getEpisodeOptionValue(episode) {
-  return `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
+  return episode.number != null ?
+    `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`
+    : episode.name;
 }
 
+// hide the bar when scrolling down
+let preScroll = -1;
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+  if (scroll >= preScroll) {
+    preScroll = scroll;
+    document.getElementById("searchContainer").style.visibility = "hidden";
+  } else {
+    document.getElementById("searchContainer").style.visibility = "visible";
+    preScroll = scroll
+  }
+
+});
+// this list  is for episodes from the api
+let Episodes = [];
+
+// fetching the api 
 window.onload = setup;
+async function data() {
+  try {
+    const res = await fetch('https://api.tvmaze.com/shows/82/episodes')
+    const response = await res.json()
+    const loader = document.getElementById('loader')
+    loader.style.display = 'block';
+    Episodes = response
+  }
+
+  catch (error) {
+    console.log('Error 404');
+    document.getElementById('error404').style.display = 'block';
+  } finally {
+    loader.style.display = 'none'
+  }
+}
+async function start() {
+  await data()
+  return Episodes
+}
